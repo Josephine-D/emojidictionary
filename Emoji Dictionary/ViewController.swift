41838054a92b06357c0de9b8cd8f4b60dd16dfb0
Josephine <josephinedonnelly@me.com>
2017-09-04 13:32:24 +1000

    Xcode beta 9-6 branch changes for swift 4

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var EmojiMainTable: UITableView!
+    
+    var emojis = ["😀", "😮 ", " 😛", "😳 ", " 🤠", " 💩" , "🐹"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+            EmojiMainTable.dataSource = self
+            EmojiMainTable.delegate = self
+        
+    }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return emojis.count
+            
+    }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            print(indexPath.row)
+            let cell = UITableViewCell()
+            cell.textLabel?.text = emojis[indexPath.row]
+            return cell
+        
     }
 
     override func didReceiveMemoryWarning() {
